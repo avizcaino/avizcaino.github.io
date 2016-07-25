ee4792886aa86991a60ab149219a4fde88a49e3a
avizcaino <avizcainoluna@gmail.com>
2016-07-25 15:29:34 +0200

    Roboto style and images

--- build/export.js
+++ build/export.js
@@ -9,11 +9,13 @@ module.exports = {
     'LICENSE',
     'dist/app-build.js',
     'dist/aurelia.js',
-    "jspm_packages/npm/bluebird@3.4.1/js/browser/bluebird.min.js", 
+    'images/**/*',
+    'jspm_packages/npm/bluebird@3.4.1/js/browser/bluebird.min.js',
     'jspm_packages/system.js',
     'jspm_packages/system-polyfills.js',
     'jspm_packages/system-csp-production.js',
-    'styles/styles.css'
+    'styles/styles.css',
+    'styles/roboto.min.css'
   ],
   // this section lists any jspm packages that have
   // unbundled resources that need to be exported.
