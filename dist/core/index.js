3b5ca3f403b2d3354b6d339c326b061367c3ac10
avizcaino <avizcainoluna@gmail.com>
2016-07-25 12:30:08 +0200

    delete dist folder

--- dist/core/index.js
+++
@@ -1,9 +0,0 @@
-define(["require", "exports"], function (require, exports) {
-    "use strict";
-    function configure(fc) {
-        fc.globalResources(['./ui/elements/title-bar.html', './ui/elements/nav-bar', './ui/elements/post-summary', './ui/elements/post-object', './ui/command/command-ext', './ui/converters/common']);
-    }
-    exports.configure = configure;
-});
-
-//# sourceMappingURL=index.js.map
