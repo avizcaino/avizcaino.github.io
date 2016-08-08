3795f43975a24e1e04aaddb8d4d46f0162a56ae4
avizcaino <avizcainoluna@gmail.com>
2016-08-08 17:37:57 +0200

    admin post-list + post-edit

--- src/routes/editor/views/post-list.ts
+++ src/routes/editor/views/post-list.ts
@@ -1,4 +1,19 @@
 /**
  * Created by alexvizcaino on 29/7/16.
  */
-export class PostList{}
+import {autoinject} from "aurelia-dependency-injection";
+import {BlogService} from "../../blog/services/blog-service";
+import {IPost} from "core-module";
+
+@autoinject
+export class PostList{
+  public posts: IPost[];
+  constructor(private _blogService: BlogService){}
+
+  activate(){
+    return this._blogService.getPosts()
+      .then((posts) => {
+        this.posts = posts;
+      })
+  }
+}
